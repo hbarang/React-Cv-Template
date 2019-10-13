a701915d8263825d624bedcd7b0209d2b5cb618e
hbarang <hbarangenc@gmail.com>
2019-10-13 10:41:18 +0300

    Updates

---
+++ precache-manifest.98c2e9f376e4a5c79b3ca242281fce23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "030966c3aa65ae52405dee6ec0aa8524",
+    "url": "/React-Cv-Template/index.html"
+  },
+  {
+    "revision": "d3b630080970f2551fb6",
+    "url": "/React-Cv-Template/static/css/2.619dee99.chunk.css"
+  },
+  {
+    "revision": "5e74810687260b892d86",
+    "url": "/React-Cv-Template/static/css/main.088d7ba3.chunk.css"
+  },
+  {
+    "revision": "d3b630080970f2551fb6",
+    "url": "/React-Cv-Template/static/js/2.b9d11d53.chunk.js"
+  },
+  {
+    "revision": "5e74810687260b892d86",
+    "url": "/React-Cv-Template/static/js/main.52406946.chunk.js"
+  },
+  {
+    "revision": "97b8abf1024f94149a57",
+    "url": "/React-Cv-Template/static/js/runtime~main.27dda787.js"
+  }
+]);
