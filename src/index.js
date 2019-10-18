7954c91cd09a8748fcce45431fa6d16ed0707c35
Christopher Coppin <christophercoppin@christophersair.home>
2019-10-19 00:04:59 +0200

    Make CvItems dynamically loaded from Data.json

--- src/index.js
+++ src/index.js
@@ -12,126 +12,82 @@ import "./styles.css";
 
 
 function App() {
-  const [experienceDisplayArray, setExperienceDisplayArray] = useState([]);
-  const [educationDisplayArray, setEducationDisplayArray] = useState([]);
-  const [projectsDisplayArray, setProjectsDisplayArray] = useState([]);
+  
+  const tempArray = [];
+  const [displayArray, setDisplayArray] = useState([]);
+  
+  Object.keys(Data).map((item, index) => {
+    tempArray.push(Data[item])
+    return null
+  })
+  
+  const dataArray = [...tempArray];
 
   /*const [displayData, setDisplayData] = useState([]);*/
-  const experienceData = Data.Experience;
-  const educationData = Data.Education;
-  const projectsData = Data.Projects;
+  
   useEffect(() => {
     function getDisplayArrays() {
-      let tempArray = [];
-      experienceData.map((item, index) => (tempArray[index] = true));
-      setExperienceDisplayArray(tempArray);
-      tempArray = [];
-      educationData.map((item, index) => (tempArray[index] = true));
-      setEducationDisplayArray(tempArray);
-      tempArray = [];
-      projectsData.map((item, index) => (tempArray[index] = true));
-      setProjectsDisplayArray(tempArray);
+      let displayArrays = [];
+      dataArray.map((data, index) => {
+        let tempArray = [];
+        data.map((item, i) => (tempArray[i] = true));
+        displayArrays[index] = tempArray;
+        return null;
+      })
+      setDisplayArray(displayArrays);
     }
     getDisplayArrays();
-  }, [experienceData, educationData, projectsData]);
-
+    // eslint-disable-next-line
+  }, []);
+  
   const reload = () =>{
-    let tempArray = [...experienceDisplayArray]
-    tempArray.map((item, index) => (tempArray[index] = true))
-    setExperienceDisplayArray(tempArray)
-
-    tempArray = [...educationDisplayArray]
-    tempArray.map((item, index) => (tempArray[index] = true))
-    setEducationDisplayArray(tempArray)
-
-    tempArray = [...projectsDisplayArray]
-    tempArray.map((item, index) => (tempArray[index] = true))
-    setProjectsDisplayArray(tempArray)
+    
+    let tempDisplayArrays = [];
+    displayArray.map((array, i) => {
+      let tempArray = [...displayArray[i]]
+      tempArray.map((item, index) => (tempArray[index] = true))
+      tempDisplayArrays.push(tempArray);
+      return null;
+    })
+    setDisplayArray(tempDisplayArrays)
   }
-
   return (
     <div className="App">
       <NavBar reloadFunction={reload} />
-      {experienceDisplayArray.every(x => !x) ? (
-        ""
-      ) : (
-        <h3 className="headers">Experience</h3>
-      )}
-      <div className="SlideContainer">
-        {experienceData.map((item, index) => {
-          return experienceDisplayArray[index] ? (
-            <CvItem
-              label={item.Label}
-              date={item.Date}
-              description={item.Description}
-              detail={item.Detail}
-              imageSrc={item.ImageSrc}
-              closeFunction={() => {
-                let tempArray = [...experienceDisplayArray];
-                tempArray[index] = false;
-                setExperienceDisplayArray(tempArray);
-              }}
-              key={index}
-            />
-          ) : (
-            ""
-          );
-        })}
-      </div>
+      
+      {displayArray.map((array, i) => {
 
-      {educationDisplayArray.every(x => !x) ? (
-        ""
-      ) : (
-        <h3 className="headers">Education</h3>
-      )}
-      <div className="SlideContainer">
-        {educationData.map((item, index) => {
-          return educationDisplayArray[index] ? (
-            <CvItem
-              label={item.Label}
-              date={item.Date}
-              description={item.Description}
-              detail={item.Detail}
-              imageSrc={item.ImageSrc}
-              closeFunction={() => {
-                let tempArray = [...educationDisplayArray];
-                tempArray[index] = false;
-                setEducationDisplayArray(tempArray);
-              }}
-              key={index}
-            />
-          ) : (
+        return (
+          array.every(x => !x) ? (
             ""
-          );
-        })}
-      </div>
-
-      {projectsDisplayArray.every(x => !x) ? (
-        ""
-      ) : (
-        <h3 className="headers">Projects</h3>
-      )}
-      <div className="SlideContainer">
-        {projectsData.map((item, index) => {
-          return projectsDisplayArray[index] ? (
-            <CvItem
-              label={item.Label}
-              date={item.Date}
-              description={item.Description}
-              detail={item.Detail}
-              imageSrc={item.ImageSrc}
-              closeFunction={() => {
-                let tempArray = [...projectsDisplayArray];
-                tempArray[index] = false;
-                setProjectsDisplayArray(tempArray);
-              }}
-              key={index}
-            />
           ) : (
-            ""
-          );
-        })}
-      </div>
+            <div key={Object.keys(Data)[i]}>
+            <h3 className="headers" >{Object.keys(Data)[i]}</h3>
+          <div className="SlideContainer">
+            {dataArray[i].map((item, index) => {
+              return (displayArray[i][index] ? (
+                <CvItem
+                  label={item.Label}
+                  date={item.Date}
+                  description={item.Description}
+                  detail={item.Detail}
+                  imageSrc={item.ImageSrc}
+                  closeFunction={() => {
+                    let tempArray = [...displayArray];
+                    tempArray[i][index] = false;
+                    setDisplayArray(tempArray);
+                  }}
+                  key={index}
+                />
+              ) : (
+                ""
+              ))
+            })}
+          </div>
+          </div>
+          )
+        )
+      })}
 
       <Footer />
     </div>
